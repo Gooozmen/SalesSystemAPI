69395b62ace66a02026f701255e217374140dc75
JGoodmen <jguzman.softdev@gmail.com>
2022-09-04 08:02:15 -0300

    all methods + employee endpont tested

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -9,23 +9,54 @@ namespace SalesSystemAPI.Controllers
     [Route("[controller]")]
     public class EmployeeController : ControllerBase
     {
-        [HttpGet(Name = "GetEmployees")]
+        [HttpGet]
+        [Route("GetAll")]
         public List<Employee> Get()
         {
             try
             {
                 return EmployeeHandler.Read();
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
                 return null;
             }
-            
         }
 
+        [HttpGet]
+        [Route("GetNameByID")]
+        public string GetNameByID(int id)
+        {
+            try
+            {
+                Employee employee = EmployeeHandler.GetById(id);
+                return employee.Name;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
 
-        [HttpPut(Name = "UpdateEmployee")]
+        [HttpGet]
+        [Route("GetEmployeeById")]
+        public Employee GetEmployee(int id)
+        {
+            try
+            {
+                return EmployeeHandler.GetById(id);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+        [HttpPut]
+        [Route("UpdateEmployee")]
         public bool Put([FromBody] PutEmployee employee)
         {
             try
@@ -47,8 +78,8 @@ namespace SalesSystemAPI.Controllers
             }
         }
 
-
-        [HttpDelete(Name = "DeleteEmployee")]
+        [HttpDelete]
+        [Route("DeleteEmployee")]
         public bool Delete([FromBody] int id)
         {
             try
@@ -57,13 +88,13 @@ namespace SalesSystemAPI.Controllers
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.Message); 
+                Console.WriteLine(ex.Message);
                 return false;
             }
         }
 
-
-        [HttpPost(Name = "CreateEmployee")]
+        [HttpPost]
+        [Route("CreateEmployee")]
         public bool Post([FromBody] PostEmployee employee)
         {
             try
@@ -76,6 +107,26 @@ namespace SalesSystemAPI.Controllers
                     Password = employee.Password,
                     Mail = employee.Mail,
                 });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        [HttpPost]
+        [Route("LogIn")]
+        public bool LogIn([FromBody] ValidateEmployee employee)
+        {
+            try
+            {
+                return EmployeeHandler.LogIn(new Employee
+                {
+                    LogOnCredential = employee.LogOnCredential,
+                    Password = employee.Password,
+                });
+
             }
             catch (Exception ex)
             {
